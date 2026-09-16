e0734677c19a7627e1c210b8764ebab00dc18064
Daniel <daniel@localhost>
2026-09-16 21:07:40 +0200

    Stand 16.09.2026 20:59 - G76-Realform, PQ Richtung, PF Schneidenradius.

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='16.09.2026 18:38 (d5ffca8b)';
+const STAND='16.09.2026 20:59 (cb2ab31e)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
