030c0a496addb2dd51090fe23c1fe5f44c6fb9f4
Daniel <daniel@localhost>
2026-09-16 22:07:42 +0200

    Stand 16.09.2026 22:07 - Schlichten: Zeilenrichtung quer zum Gefaelle, Z-konstante Bahn fuer steile Flanken.

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='16.09.2026 20:59 (cb2ab31e)';
+const STAND='16.09.2026 22:07 (4c13fd5b)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
