5090ba81c74c8af5d43c431effad421484f22547
Daniel <daniel@localhost>
2026-09-19 19:35:22 +0200

    Fraesen: Zeiten im Arbeitsplan und Kalkulation (19.09.2026)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='19.09.2026 09:58 (936cb991)';
+const STAND='19.09.2026 19:35 (30a0e409)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
