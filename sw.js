8aebf2bb7b4332154176a8585e3e5d7834f1c2e3
Daniel <daniel@localhost>
2026-09-20 13:49:28 +0200

    DZ CAM Fraesen: Rueckseite der Form (Absaugkammer, Spannkante)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='20.09.2026 13:41 (48958549)';
+const STAND='20.09.2026 13:49 (9c21a3e5)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
