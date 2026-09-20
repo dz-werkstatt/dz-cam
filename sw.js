673448f863df56d3667d8e8134e22be7be5607b8
Daniel <daniel@localhost>
2026-09-20 15:47:50 +0200

    DZ CAM Fraesen: Werkzeug in der Simulation dreifarbig - Schneide gold, Schaft silber, Halter schwarz

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='20.09.2026 15:35 (98566832)';
+const STAND='20.09.2026 15:47 (ec999f24)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
