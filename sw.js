adb78c4768c0cb172db9ad6971055b4b23f73163
Daniel <daniel@localhost>
2026-09-20 11:08:31 +0200

    DZ CAM Fraesen: Stand 20.09.2026 11:08 - DZ24 im Programm-Test belegt, Schrittkopf mit deutschem Text lesbar

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='20.09.2026 09:29 (90839e22)';
+const STAND='20.09.2026 11:08 (bb8f2cd6)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
