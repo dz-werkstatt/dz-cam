5ccebe7185f7168ab83be40925d5f121aa8f8bbe
Daniel <daniel@localhost>
2026-09-20 13:25:05 +0200

    DZ CAM Fraesen: die Formschraege korrigieren (Stand 20.09.2026 13:24)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='20.09.2026 12:55 (2fc4b46b)';
+const STAND='20.09.2026 13:24 (9e271df7)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
