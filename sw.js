e2a7cd5b579be3a6d7781d74a3aaca5f835284e0
Daniel <daniel@localhost>
2026-09-22 23:32:23 +0200

    Stand 22.09.2026: Eckenradiusfraeser

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='22.09.2026 22:35 (9f950a0f)';
+const STAND='22.09.2026 23:32 (1b8ce40e)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
