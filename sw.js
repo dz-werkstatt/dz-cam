154719508f0440fdbf700cb69dc27eb10971dd4f
Daniel <daniel@localhost>
2026-09-23 01:18:04 +0200

    Stand 23.09.2026: Stufenhoehe

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='23.09.2026 00:48 (ccd87d87)';
+const STAND='23.09.2026 01:18 (d5bd6a9b)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
