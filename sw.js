b9d33d3ca73506367dc7dd190bfa9abfe4f0d8eb
Daniel <daniel@localhost>
2026-09-23 11:58:08 +0200

    Stand 23.09.2026: Konturschnitt, Vorschub nach Eingriff, Bahnecken verrunden, aequidistant schlichten

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='23.09.2026 09:51 (72972af0)';
+const STAND='23.09.2026 11:57 (5a46ec5f)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
