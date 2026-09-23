86f400afdede970ff6852a8220db316e9324ae43
Daniel <daniel@localhost>
2026-09-23 21:12:06 +0200

    Stand 23.09.2026: Muendung in der Werkzeugwahl

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='23.09.2026 20:14 (68221ea1)';
+const STAND='23.09.2026 21:12 (8f81fe66)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
