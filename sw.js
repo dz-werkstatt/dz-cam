4ba6a9c9b4368a40be1511133bc63b07fc41eee2
Daniel <daniel@localhost>
2026-09-23 19:42:43 +0200

    Stand 23.09.2026: Werkzeugplaetze 1-16, Rampen-Ueberlauf beim Schlichten

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='23.09.2026 11:57 (5a46ec5f)';
+const STAND='23.09.2026 19:42 (5c364635)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
