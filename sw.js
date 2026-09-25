fc3f7a3e06077258eb410c5dd5ad7006d035358b
Daniel <daniel@localhost>
2026-09-25 09:47:36 +0200

    Stand 25.09.2026: G77 Plangewinde der 1500er, gemeinsame WZ-KORR

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='24.09.2026 17:12 (1f9b7255)';
+const STAND='25.09.2026 09:47 (9600fa9e)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
