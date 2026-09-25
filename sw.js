96641ed1920e002ecea26e8d61be654aed310c73
Daniel <daniel@localhost>
2026-09-25 10:08:25 +0200

    Stand 25.09.2026: Bohrzyklus mit G95 F

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='25.09.2026 09:47 (9600fa9e)';
+const STAND='25.09.2026 10:08 (1f048394)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
