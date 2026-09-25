52ce8f15d98cf36535914371e735990b0a9ad044
Daniel <daniel@localhost>
2026-09-25 21:19:39 +0200

    Vektor-PDF als Zeichnungsunterlage (Stand 25.09.2026)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='25.09.2026 18:34 (a79f2cfa)';
+const STAND='25.09.2026 21:19 (e774fdd8)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
