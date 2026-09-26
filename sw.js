dcda7b7b1b9150a81a06bce98e134d660ec411a5
Daniel <daniel@localhost>
2026-09-26 12:19:35 +0200

    Stand 26.09.2026 12:19: zweite Aufspannung (Fraesen)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='26.09.2026 10:37 (a5b8d893)';
+const STAND='26.09.2026 12:19 (73c43235)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
