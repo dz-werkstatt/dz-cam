4d303a11d0fe56553ff3d182b87d4f3992408775
Daniel <daniel@localhost>
2026-09-26 08:56:50 +0200

    Halter als Kollisionskoerper (Stand 26.09.2026)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='25.09.2026 23:18 (d3af888c)';
+const STAND='26.09.2026 08:56 (98e35441)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
