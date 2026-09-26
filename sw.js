7725c2cf736f7a5619c3b2328400ddb689746de6
Daniel <daniel@localhost>
2026-09-26 17:55:08 +0200

    Fraesen: 3+2 angestellt (PLANE SPATIAL / CYCLE800)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='26.09.2026 17:06 (b54813ac)';
+const STAND='26.09.2026 17:55 (cac06b51)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
