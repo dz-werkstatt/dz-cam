c7593188c58d079f466354a2db6652e802c8ef09
Daniel <daniel@localhost>
2026-09-26 10:37:48 +0200

    Fraesen: Merkmale aus den Boeden als eigene Zyklen (26.09.2026)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // 15.09.2026: 'immer noch der Stand vom 12.09.'). Jetzt aendert jede
 // Veroeffentlichung diese Zeile, der Worker installiert neu und holt
 // alle Dateien frisch.
-const STAND='26.09.2026 08:56 (98e35441)';
+const STAND='26.09.2026 10:37 (a5b8d893)';
 const CACHE='dzcam-'+STAND.replace(/[^0-9a-f]/gi,'');
 // KEIN './' in der Vorcache-Liste: nicht jeder Server liefert einen
 // Verzeichnis-Index, und EIN Fehlschlag laesst addAll die GANZE
